6e9c3d30f8991c7be7a1ae8918fd0a90d2f8745c
Chris Baird <chrisbaird05@icloud.com>
2018-11-17 11:48:21 -0800

    Added Interfaces example

--- greeter.js
+++ greeter.js
@@ -1,7 +1,12 @@
+// Type annotations
 // Using TypeScript you can declare data types
 function greeter(person) {
     return 'Hello,' + person;
 }
 var user = 'Bob Smith';
 document.body.innerHTML = greeter(user);
-// Compile TypeScript in terminal 'tsc filename.ts'
+function greeter2(person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+var user2 = { firstName: 'Chris', lastName: 'Baird' };
+console.log(user2);
--- greeter.ts
+++ greeter.ts
@@ -1,4 +1,4 @@
-
+// Type annotations
 // Using TypeScript you can declare data types
 function greeter(person: string){
     return 'Hello,' + person;
@@ -9,3 +9,17 @@ let user = 'Bob Smith';
 document.body.innerHTML = greeter(user);
 
 // Compile TypeScript in terminal using the command:'tsc filename.ts'
+
+// Interfaces
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter2(person: Person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+
+let user2 = { firstName: 'Chris', lastName: 'Baird'};
+
+console.log(user2);
